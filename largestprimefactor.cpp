adc56b8ea65fdd1186fe1439ecf803e58acbfbc1
Gopika Murali <gopikamurali.mec@gmail.com>
2020-06-12 18:14:36 +0530

    TLE

--- largestprimefactor.cpp
+++ largestprimefactor.cpp
@@ -1,15 +1,12 @@
-#include <iostream>
-#include <math.h>
-using namespace std;
 
-bool prime(long long x){
+bool prime(int x){
     if(x==0){
         return 0;
     }
     if(x==1){
         return 0;
     }
-    for(long long i=2; i<=sqrt(x);i++){
+    for(int i=2; i<=sqrt(x);i++){
         if(x%i==0){
             return 0;
         }
@@ -20,23 +17,22 @@ bool prime(long long x){
 
 int main() {
 	//code
-	long t;
-	long long n;
+	int t;
+	long n,ar[1000];
 	cin>>t;
 	while(t--){
 	    cin>>n;
-	    long long max=0;
-        for(long long i=2; i<=n/2;i++){
-            if((n%i==0) && prime(i) && i>max){
-                        max=i;
-                    }
+	    int j=0;
+        for(int i=2; i<=n/2;i++){
+            if(n%i==0){
+                ar[j++]=i;
+            }
         }
-	   // for(int i=j-1; i>0;i--){
-	   //     if(prime(ar[i])){
-	   //         cout<<ar[i]<<"\n";
-	   //     }
-	   // }
-	   cout<<max<<"\n";
+	    for(int i=j-1; i>0;i--){
+	        if(prime(ar[i])){
+	            cout<<ar[i]<<"\n";
+	        }
+	    }
 	}
 	return 0;
 }
