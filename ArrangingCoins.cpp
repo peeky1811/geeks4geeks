3d1281f043b29a7594b685332bc581f81bcdbec0
Gopika Murali <gopikamurali.mec@gmail.com>
2020-07-01 15:00:38 +0530

    Arranging Coins in LeetCode
    You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
    
    Given n, find the total number of full staircase rows that can be formed.
    
    n is a non-negative integer and fits within the range of a 32-bit signed integer.
    
    Example 1:
    
    n = 5
    
    The coins can form the following rows:
    ¤
    ¤ ¤
    ¤ ¤
    
    Because the 3rd row is incomplete, we return 2.
    Example 2:
    
    n = 8
    
    The coins can form the following rows:
    ¤
    ¤ ¤
    ¤ ¤ ¤
    ¤ ¤
    
    Because the 4th row is incomplete, we return 3.

--- ArrangingCoins.cpp
+++ ArrangingCoins.cpp
@@ -1,22 +1,21 @@
-#include<iostream>
-
-int arrangeCoins(int n){
-  if(n==0)  //corner case
-    return 0;
-  while(n){
-    if((n-i)<0)
-      i--;
-      break;
-    if((n-i)==0)
-      break;
-    n=n-i;
-    i++;
-  }
-  return i;
-}
-
-int main(){
-  int n;
-  cin>>n;
-  cout<<arrangeCoins(n);
-}
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i=1;
+        if(n==0){//corner case
+            return 0;
+        }
+        while(n){
+            if((n-i)<0){
+                i--;
+                break;
+            }
+            if((n-i)==0){
+                break;
+            }
+            n=n-i;
+            i++;
+        } 
+        return i;
+    }
+};
